0fa3a12db13df83d1b7d8b67e3bf74a13ca77991
Johnson Liu <jyliu17@gmail.com>
2020-08-28 23:45:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,6 +22,10 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  literal_aoh = [
+    
+    ]
+  
 end
 
 def aoh_lookup(aoh, row, key)
